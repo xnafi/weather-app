fa8aee8bf16aa792053c1813b966229440cd0587
Forhad Khan <fk.nafi11@gmail.com>
2022-09-05 19:38:35 +0600

    update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 const loadWeatherData = (city) => {
     const API_KEY = 'b1579f502865aec423c1f4c8b8a45401'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     fetch(url)
         .then(res => res.json())
         .then(data => getWeatherData(data))
 }
 const getWeatherData = (data) => {
-    const cityName = document.getElementById('city-name').innerText = data.name;
-    const cityTemp = document.getElementById('temp').innerText = data.main.temp;
-    const cityWeaterCondition = document.getElementById('condition').innerText = data.weather[0].main;
+    console.log(data);
+    const cityName = document.getElementById('city-name').innerText = `${data.name ? data.name : 'no city found'}`;
+    const cityTemp = document.getElementById('temp').innerText =`${ data.main.temp ?  data.main.temp  : 'no data'}`;
+    const cityWeaterCondition = document.getElementById('condition').innerText = `${data.weather[0].main  ? data.weather[0].main  : 'no updates'}` ;
     console.log(cityWeaterCondition);
 }
 
